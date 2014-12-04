2ca95dfe4c90553c9b2aa7372e73b22edae68c13
root <han.veiga@gmail.com>
2014-12-04 09:14:41 -0500

    changed urls for collect

---
+++ WhatchaUpTov1/collect/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import patterns, url
+from django.views.generic import TemplateView
+
+from collect import views
+from django.conf import settings
+
+
+urlpatterns = patterns('',
+    #url(r'^$', views.index.as_view(), name='index'),
+    url(r'^$', TemplateView.as_view(template_name='index.php'),
+    url(r'^post', views.buttonExample),
+)
