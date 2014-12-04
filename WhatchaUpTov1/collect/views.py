07702957ecd156f78ffcb8ec490b20282eef4de3
root <han.veiga@gmail.com>
2014-12-04 09:13:39 -0500

    added generic landing page

--- WhatchaUpTov1/collect/views.py
+++ WhatchaUpTov1/collect/views.py
@@ -1,8 +1,25 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.views.generic import View
 from django.views.generic import TemplateView
 from django.conf.urls import patterns
 
-class IndexView(TemplateView):
-    template_name = 'templates/index.php'
+from django.views.decorators.csrf import csrf_exempt
+
+#class index(TemplateView):
+#    template_name = 'index.php'
+
+class index(View):
+	template_name = 'index_test.php'
+
+@csrf_exempt
+def buttonExample(request):
+    print 'RECEIVED REQUEST: ' + request.method
+    if request.method == 'POST':
+        print 'Hello'
+        print 'dude'
+        print request
+        return HttpResponseRedirect('http://google.com') 
+    else: #GET
+        print 'Nothing'
+        return HttpResponseRedirect('http://google.com') 
