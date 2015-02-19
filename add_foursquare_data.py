0c5f5ded363d92ced01d451867bb1d7da0501b08
hanveiga <han.veiga@gmail.com>
2015-02-19 18:06:43 +0100

    changed the input output files to be passed through sys

--- add_foursquare_data.py
+++ add_foursquare_data.py
@@ -2,36 +2,9 @@ import foursquare_checkin as fs
 from users import *
 import tweepy
 import numpy as np
+import sys
 
-"""jb = pickle.load(open('data/hiyaholls.pkl', 'rb'))
-
-venues_list = []
-check_in_dates = []
-
-for check_in in jb.foursquare.check_ins:
-	try:
-		_, ven_id = fs.get_venue_from_checkin(check_in.entities['urls'][0]['expanded_url'])
-		#print check_in.entities['urls'][0]['expanded_url']
-		try:
-			check_in_dates.append(check_in.created_at)
-			venue = fs.get_venue(ven_id) 
-			#print venue
-			venues_list.append([check_in, venue])
-		except:
-			# venue id has a problem
-			venues_list.append([check_in, 'null'])
-			pass
-	except:
-		# venue doesn't exist
-		venues_list.append([check_in, 'null'])
-		pass
-
-jb.foursquare.check_ins = venues_list
-
-print jb.foursquare.check_ins[0][0].text.encode('cp850', errors='replace').decode('cp850')
-print jb.foursquare.check_ins[0][1]['venue']['name']
-"""
-def fill_foursquare_data(pickle_name):
+def fill_foursquare_data(pickle_name, output_filepath):
 	jb = pickle.load(open('data/'+ pickle_name + '.pkl', 'rb'))
 	venues_list = []
 	check_in_dates = []
@@ -56,22 +29,22 @@ def fill_foursquare_data(pickle_name):
 
 	print len(venues_list)
 	jb.foursquare.check_ins = venues_list
-	pickle.dump(jb,open('data/post/'+pickle_name+'.pkl','wb'))	
+	pickle.dump(jb,open(output_filepath+pickle_name+'.pkl','wb'))	
 
-def process_files(filepath):
-	filenames = os.listdir(filepath)
-	existing_files = os.listdir(filepath+'post/')
+def process_files(input_filepath):
+	filenames = os.listdir(input_filepath)
+	existing_files = os.listdir(input_filepath+'post/')
 	for filename in filenames:
 		print filename
 		if filename in existing_files:
 			"file exists"
 			continue
 
-		info = os.stat(filepath+filename)
+		info = os.stat(input_filepath+filename)
 		if info.st_size > 4000 and filename!='post':
 	 		nameoffile = re.split('\.',filename)[0]
 			print nameoffile
-			fill_foursquare_data(nameoffile)
+			fill_foursquare_data(nameoffile, output_filepath)
 
 if __name__ == '__main__':
-	process_files('C:/Users/Maria/Desktop/thesis/data/')
+	process_files(sys.argv[1],sys.argv[2])
